acc5c5522bb9a814130b3d80a347dd9ba3a7a97b
Pratiksha <thepolicearecomingdawg@gmail.com>
2026-09-07 03:30:53 +0800

    Level-0 : Rename, Greet, Exit

---
+++ src/main/java/MARY.java
@@ -0,0 +1,19 @@
+public class MARY {
+    public static void main(String[] args) {
+        String separator = "____________________________________________________________";
+        String banner = "███╗   ███╗ █████╗ ██████╗ ██╗   ██╗\n"
+                + "████╗ ████║██╔══██╗██╔══██╗╚██╗ ██╔╝\n"
+                + "██╔████╔██║███████║██████╔╝ ╚████╔╝\n"
+                + "██║╚██╔╝██║██╔══██║██╔══██╗  ╚██╔╝\n"
+                + "██║ ╚═╝ ██║██║  ██║██║  ██║   ██║\n"
+                + "╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝";
+
+        System.out.println(separator);
+        System.out.println(banner);
+        System.out.println("Hello! I'm MARY.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
