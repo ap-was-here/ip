0e11e69156d8b14a18261f034ad9905c61c53bf5
Pratiksha <thepolicearecomingdawg@gmail.com>
2026-09-14 17:46:17 +0800

    Level-1 : Echo

--- src/main/java/MARY.java
+++ src/main/java/MARY.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class MARY {
     public static void main(String[] args) {
-        String separator = "____________________________________________________________";
+        String separator = "_._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._";
         String banner = "███╗   ███╗ █████╗ ██████╗ ██╗   ██╗\n"
                 + "████╗ ████║██╔══██╗██╔══██╗╚██╗ ██╔╝\n"
                 + "██╔████╔██║███████║██████╔╝ ╚████╔╝\n"
@@ -10,10 +12,24 @@ public class MARY {
 
         System.out.println(separator);
         System.out.println(banner);
-        System.out.println("Hello! I'm MARY.");
-        System.out.println("What can I do for you?");
+        System.out.println("Hi! I'm MARY.");
+        System.out.println("What have you got for me today?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            System.out.println(separator);
+            if (command.equals("bye")) {
+                System.out.println(" Bye. Complete your tasks on time!");
+                System.out.println(separator);
+                break;
+            }
+
+            // MARY echoes every command so the user can confirm what was entered.
+            System.out.println(" " + command);
+            System.out.println(separator);
+        }
     }
 }
